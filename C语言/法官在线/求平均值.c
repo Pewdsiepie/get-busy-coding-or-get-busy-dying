c2fde736826cb5f7a9320b1f3fb970ace14b2330
PewsDiePie <56794828+PewsDiePie@users.noreply.github.com>
2019-11-05 22:23:54 +0800

    Create 求平均值.c
    Description
    求N个数的平均值。
    Input
    第一行数字N（ 1 <= N <= 100 ）。
    第二行是以空格间隔的N个实数。
    Output
    平均值（保留2位小数）
    Sample Input
    5
    1.5 3 6 9 4
    Sample Output
    4.70

---
+++ C语言/法官在线/求平均值.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+    float sum,x,i,d,c;
+    scanf("%f",&x);
+    d=-1;
+    c=-1;
+    sum=x;
+    i=2;
+    while(fabs(c)>=0.00001)
+    {
+      c=x/i;
+      c=c*d;
+      d=-1*d;
+      sum=sum+c;
+      i++;
+    }
+    printf("%.2f",sum);
+}
