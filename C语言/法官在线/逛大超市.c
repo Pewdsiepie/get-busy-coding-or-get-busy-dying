218bf186885b520fe56c4cfc8a88dd21f8383ba2
PewsDiePie <56794828+PewsDiePie@users.noreply.github.com>
2019-11-15 23:53:28 +0800

    Update 逛大超市.c

--- C语言/法官在线/逛大超市.c
+++ C语言/法官在线/逛大超市.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 int main()
 {
-printf("40\n");
-return 0;
+    int N,a,i;
+    double M=0;
+    scanf("%d",&N);
+    for(i=1;i<=N;i++)
+    {
+        scanf("%d",&a);
+        M=M+a;
+    }
+    if(M>=500) M=0.8*M;
+    else if(M>=300) M=0.9*M;
+    else M=M;
+    printf("%.2lf",M);
+    return 0;
+
 }
